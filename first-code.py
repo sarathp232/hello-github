d43d0c550e8d39654077621e2ef7c8f6cb0b73b1
sarathp232 <sarathp232@gmail.com>
2025-08-05 10:48:08 +0530

    Reverted accidental mistake in previous commit

--- first-code.py
+++ first-code.py
@@ -1,1 +1,3 @@
-print("This is my change from the 'new-message' branch.)
+print("Hello GitHub, I'm learning version control!")
+print("This is my second GitHub edit. Still going strong!")
+print("This is my change from the 'new-message' branch.")
